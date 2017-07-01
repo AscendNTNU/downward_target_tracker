9d61649dd6ada7c5809326dcd239620093aebe5a
Simen H <simenh.jobb@gmail.com>
2017-07-01 20:13:40 +0200

    view_color uses imgui rendering

--- src/view_color.cpp
+++ src/view_color.cpp
@@ -11,9 +11,12 @@ void view_color(latest_image_t latest_image, downward_target_tracker::info lates
     float g_r = latest_info.g_r;
     float g_b = latest_info.g_b;
     float g_n = latest_info.g_n;
-    vdbOrtho(-1.0f, +1.0f, +1.0f, -1.0f);
-    vdbSetTexture2D(0, I, Ix, Iy, GL_RGB, GL_UNSIGNED_BYTE, GL_NEAREST, GL_NEAREST);
-    vdbDrawTexture2D(0);
+    static unsigned char *I_groups = 0;
+    if (!I_groups)
+        I_groups = (unsigned char*)malloc(Ix*Iy*3);
+
+    for (int i = 0; i < 3*Ix*Iy; i++)
+        I_groups[i] = I[i];
 
     // Find connected components belonging of top plate sections
     int *points;
@@ -28,57 +31,39 @@ void view_color(latest_image_t latest_image, downward_target_tracker::info lates
     opt.g_n = g_n;
     groups = cc_find_top_plates(I, Ix, Iy, opt, &points, &num_points);
 
-    // draw connected components
+    // draw connected pixels
     {
-        #if 0
-        int max_n = 0;
-        for (int i = 0; i < groups.count; i++)
-        {
-            if (groups.group_n[i] > max_n)
-                max_n = groups.group_n[i];
-        }
-        #endif
-
-        vdbOrtho(0.0f, Ix, Iy, 0.0f);
-        glBegin(GL_TRIANGLES);
         for (int i = 0; i < num_points; i++)
         {
             int p = points[i];
-            int x = p % Ix;
-            int y = p / Ix;
             int l = groups.label[p];
-            int n = groups.group_n[l];
-
-            #if 0
-            if (n > 0.025f*max_n)
-            #endif
-            {
-                glColor4f(vdbPalette(l));
-                vdbFillRect(x, y, 1.0f, 1.0f);
-            }
+            vdb_color c = vdbPalette(l);
+            I_groups[3*p+0] = (unsigned char)(c.r*255.0f);
+            I_groups[3*p+1] = (unsigned char)(c.g*255.0f);
+            I_groups[3*p+2] = (unsigned char)(c.b*255.0f);
         }
-        glEnd();
-
-        vdbAdditiveBlend();
-        glLines(2.0f);
-        glColor4f(0.2f, 0.8f, 1.0f, 1.0f);
-        for (int i = 0; i < groups.count; i++)
-        {
-            #if 0
-            // if (groups.group_n[i] > 0.025f*max_n)
-            #endif
-            {
-                float min_x = groups.group_min_x[i];
-                float min_y = groups.group_min_y[i];
-                float max_x = groups.group_max_x[i];
-                float max_y = groups.group_max_y[i];
-                vdbDrawRect(min_x+0.5f, min_y+0.5f, max_x-min_x, max_y-min_y);
-            }
-        }
-        glEnd();
-        vdbAlphaBlend();
+        vdbOrtho(-1.0f, +1.0f, +1.0f, -1.0f);
+        vdbSetTexture2D(0, I_groups, Ix, Iy, GL_RGB, GL_UNSIGNED_BYTE, GL_NEAREST, GL_NEAREST);
+        vdbDrawTexture2D(0);
     }
 
+    // draw bounding boxes
+    {
+        ImBegin();
+        ImLineWidth(2.0f);
+        ImColor4f(0.2f, 0.8f, 1.0f, 1.0f);
+        for (int i = 0; i < groups.count; i++)
+        {
+            float min_x = groups.group_min_x[i];
+            float min_y = groups.group_min_y[i];
+            float max_x = groups.group_max_x[i];
+            float max_y = groups.group_max_y[i];
+            ImDrawRect(min_x+0.5f, min_y+0.5f, max_x+0.5f, max_y+0.5f);
+        }
+        ImEnd();
+    }
+
+    // gui
     {
         using namespace ImGui;
         SetNextWindowSize(ImVec2(400, 300), ImGuiSetCond_Appearing);
